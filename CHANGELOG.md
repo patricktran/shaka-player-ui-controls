5feeedbc9038ce3731388c9a38e752f4d924abf0
Patrick Tran <patrick.tran43@gmail.com>
2022-01-17 16:54:58 -0800

    reduce build bundle size

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
-# 1.0.4 (2022-01-17)
+# 1.0.5
+
+Optimize build bundle
+
+# 1.0.4
 
 Fix styling bug with vertical volume slider
 
-# 1.0.3 (2022-01-17)
+# 1.0.3
 
 Improve CSS selector scoping
 
-# 1.0.1 (2022-01-16)
+# 1.0.1
 
 Initial release
--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import path from 'path';
 import pkg from './package.json';
 
@@ -20,6 +21,7 @@ export default
         }
     ],
     plugins: [
+        peerDepsExternal(),
         postcss({
             include: "**/styles.css",
             extract: path.resolve("dist/main.css"),
